6e8e141d412a3468f11eef91239e93353730fe3d
jessicapardo <jessica.m.pardo@gmail.com>
2021-03-09 11:43:27 -0600

    Final app

---
+++ static/js/app.js
@@ -0,0 +1,119 @@
+function getName() {
+    // Select dropdown menu id and assign it to a variable
+    let dropdownMenu = d3.select('#selDataset');
+    // Read "names" values from json file and append into dropdown menu
+    d3.json('/data/samples.json')
+        .then(subject => subject.names
+            .forEach(name => dropdownMenu
+                .append('option')
+                .text(name)
+                .property('value'),
+
+                // Initialize page with default data 
+                getBoxdata(subject.names[0]),
+                getBar(subject.names[0]),
+                getBubble(subject.names[0]),
+                getGauge(subject.names[0])
+            ),
+        );
+};
+
+// // Fetching new data each time a new sample is selected
+function optionChanged(id) {
+    getBoxdata(id);
+    getBar(id);
+    getBubble(id);
+    getGauge(id);
+};
+
+// =============================================
+// Demographic Info Box
+function getBoxdata(id) {
+    //Read "metadata" from json file for each subject and assign it to a variable
+    d3.json('/data/samples.json').then(data => {
+            let subjectData = data.metadata
+                .filter(subject => subject.id.toString() === id)[0];
+
+            // Select demographic info id and assign it to a variable
+            let subjectMetadata = d3.select('#sample-metadata');
+            
+            subjectMetadata.html('');
+            // Push data 
+            Object.entries(subjectData)
+                .forEach(([key, value]) => subjectMetadata
+                    .append('p')
+                    .text(`${key}: ${value}`),
+                );
+        });
+};
+
+
+getName();
+
+// =============================================
+// Bar chart
+function getBar(id) {
+    // Read data from json file for each sample
+    d3.json('/data/samples.json').then(data => {
+            let sortedSample = data.samples.filter(sample => sample.id === id)[0];
+            console.log(sortedSample);
+            // Trace for bar chart that displays each sample top 10 OTU values
+            let barTrace = {
+                x: sortedSample.sample_values.slice(0, 10).reverse(),
+                y: sortedSample.otu_ids.slice(0, 10).map(otuid => `OTU ${otuid}`).reverse(),
+                text: sortedSample.otu_labels.slice(0, 10).reverse(),
+                type: 'bar',
+                orientation: 'h'
+            };
+            // Data
+            data = [barTrace];
+            // Layout
+            let layout = {
+                title: { text: `Top 10 OTU for Test Subject No. ${id}`, font: {size: 20} },
+                height: 600,
+                width: 500,
+                xaxis: { title: "Sample Value" },
+                yaxis: { title: "Bacteria ID" },
+                
+            };
+            
+            Plotly.newPlot('bar', data, layout);
+        });
+};
+
+// =============================================
+// Bubble chart
+function getBubble(id) {
+    // Read data from json file for each sample, assign it to a variable, and plot it
+    d3.json('/data/samples.json').then(data => {
+            let sortedSample = data.samples.filter(sample => sample.id === id)[0];
+            console.log(sortedSample);
+
+            // Trace for bubble chart 
+            let bubbleTrace = {
+                x: sortedSample.otu_ids,
+                y: sortedSample.sample_values,
+                text: sortedSample.otu_labels,
+                mode: "markers",
+                marker: {
+                    size: sortedSample.sample_values,
+                    color: sortedSample.otu_ids,
+                    colorscale: 'Earth',
+                    opacity: 0.8
+                },
+            };
+            // Data
+            data = [bubbleTrace];
+            // Layout
+            let layout = {
+                title: { text: `Test Subject No. ${id} Belly Button Biodiversity`,font: {size: 24, color: 'black'}},
+                height: 600,
+                width: 1200,
+                xaxis: { title: 'OTU ID (Bacteria)'},
+                yaxis: { title: 'Sample Value', automargin: true, },
+            
+            };
+                    
+            Plotly.newPlot('bubble', data, layout);
+        });
+};
---
+++ static/js/bonus.js
@@ -0,0 +1,54 @@
+// =============================================
+// Gauge chart
+function getGauge(id) {
+    // Read "metadata" from json file for each subject and assign it to a variable
+    d3.json('/data/samples.json').then(data => {
+        let subjectData = data.metadata.filter(subject => subject.id.toString() === id)[0];
+        console.log(subjectData);
+
+        // Extract data and assign it to a variable 
+        let value = subjectData.wfreq;
+        console.log(value);
+
+        // Data
+        data = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                type: 'indicator',
+                mode: 'gauge+number+delta',
+                value: value,
+                title: { text: 'Belly Button Washing Frequency <br><i>Scrubs per Week</i>', font: { size: 20 } },
+                gauge: {
+                    axis: { range: [null, 9], tickwidth: 1 },
+                    bar: { color: 'red', thickness: 0.3 },
+                    axes: [{
+                        pointers: [{
+                            value: 80,
+                            type: 'Marker',
+                            markerType: 'Circle'
+                        }]
+                    }],
+                    steps: [
+                        { range: [0, 1], color: '#c2ddff' },
+                        { range: [1, 2], color: '#8fc1ff' },
+                        { range: [2, 3], color: '#5ca5ff' },
+                        { range: [3, 4], color: '#2989ff' },
+                        { range: [4, 5], color: '#0777ff' },
+                        { range: [5, 6], color: '#0067e4' },
+                        { range: [6, 7], color: '#0057c2' },
+                        { range: [7, 8], color: '#00397e' },
+                        { range: [8, 9], color: '#001a3a' }
+                    ],
+                },
+            },
+        ];
+        // Layout 
+        let layout = {
+            width: 400,
+            height: 360,
+            margin: { t: 108, r: 20, l: 20 },
+        };
+
+        Plotly.newPlot('gauge', data, layout);
+    });
+};
